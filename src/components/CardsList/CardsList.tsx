6fff66729c5abef6748cd219972c5b0f9e4b28bd
Evgenii Zakirov <e.zakirov81@outlook.com>
2024-05-08 12:09:37 -0600

    build

--- src/components/CardsList/CardsList.tsx
+++ src/components/CardsList/CardsList.tsx
@@ -1,16 +1,22 @@
-import React from "react";
-import { ICard } from "../MainPage/MainPage";
+import React from 'react'
+//import { ICard } from "../MainPage/MainPage";
+
+interface ICard {
+  id: number
+  value: number
+  checked: boolean
+}
 
 const card = (item: ICard) => (
   <div key={item.id} data-name="card" className="w-32 mx-3">
     <div
       className={`border ${
-        item.checked ? "border-fronk-green" : "border-fronk-orange"
+        item.checked ? 'border-fronk-green' : 'border-fronk-orange'
       }`}
     >
       <div
         className={`mt-3 mx-3 flex flex-col justify-center items-center ${
-          item.checked ? "bg-fronk-green" : "bg-fronk-orange"
+          item.checked ? 'bg-fronk-green' : 'bg-fronk-orange'
         }`}
       >
         <p className="text-center text-6xl font-semibold italic mt-5 px-3">
@@ -20,12 +26,12 @@ const card = (item: ICard) => (
       </div>
       <div className="text-center">
         <p className="text-2xl my-1">
-          {item.checked ? "done" : `day ${item.id}`}
+          {item.checked ? 'done' : `day ${item.id}`}
         </p>
       </div>
     </div>
   </div>
-);
+)
 
 const CardsList = ({ list }: { list: ICard[] }) => {
   return (
@@ -39,13 +45,13 @@ const CardsList = ({ list }: { list: ICard[] }) => {
       {/* grid grid-rows-4 grid-flow-col gap-4 */}
       {/* <div className="grid grid-cols-7 gap-4">{list.map((item) => card(item))}</div> */}
       <div className="flex justify-between w-full mt-3 overflow-x-auto">
-        {list.map((item) => card(item))}
+        {list.map(item => card(item))}
       </div>
       <button className="w-5/12 font-archivo text-5xl font-light border border-fronk-orange hover:bg-fronk-orange uppercase py-2.5 mt-10 mb-5">
         Claim
       </button>
     </div>
-  );
-};
+  )
+}
 
-export default CardsList;
+export default CardsList
